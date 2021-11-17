bb9ba2e938c1a9fe9cacdca8e45555febba0ec51
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:18:06 +0200

    kaka

---
+++ Kuud/Kuud/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Kuud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string autummFile = @"C:\Users\opilane\samples\autumm.txt";
+            string winterFile = @"C:\Users\opilane\samples\winter.txt";
+
+            string[] winterData = File.ReadAllLines(autummFile);
+            string[] autummData = File.ReadAllLines(winterFile);
+
+            File.ReadAllLines(autummFile, autummData);
+            File.ReadAllLines(winterFile, winterData);
+
+
+
+        }
+    }
+}
---
+++ RandomQuetes/RandomQuetes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomQuetes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\users\opilane\samples\quotes.txt"
+            string[] DataFromFile = File.ReadAllLines(filePath);
+
+            Console.WriteLine(DataFromFile[0]);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, DataFromFile.Length);
+            Console.WriteLine(DataFromFile[randomIndex]);
+                                                   
+
+        }
+    }
+}
